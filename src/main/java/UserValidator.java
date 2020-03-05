f6edae1345cf24452ade8ea381af67d7c87e9c26
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-05 16:15:09 +0530

    MODIFIED:Added function to validate mobile number

--- src/main/java/UserValidator.java
+++ src/main/java/UserValidator.java
@@ -3,6 +3,7 @@ import java.util.regex.Pattern;
 
 public class UserValidator
 {
+    String COUNTRY_CODE="91";
     // Method for validating first name using Regex
     public boolean validateFirstName(String firstName)
     {
@@ -25,6 +26,14 @@ public class UserValidator
         String patternForEmailId="^[a-z]{1,}([.]?[-]?[+]?[a-z0-9]{1,})?[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,4}([.]?[a-z]{2,4})?$";
         Pattern p = Pattern.compile(patternForEmailId);
         Matcher matcher = p.matcher(emailId);
+        return matcher.matches();
+    }
+    // Method for validating mobile number using Regex
+    public boolean validateMobileNumber(String mobileNumber)
+    {
+        String patternForMobileNumer="^"+COUNTRY_CODE+"[ ][9876]{1}[0-9]{9}$";
+        Pattern p = Pattern.compile(patternForMobileNumer);
+        Matcher matcher = p.matcher(mobileNumber);
         return matcher.matches();
     }
 }
