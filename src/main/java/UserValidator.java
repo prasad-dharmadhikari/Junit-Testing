dc5f4e6c675d4ccd0a8a0edf627b39221f348b2a
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-05 14:49:44 +0530

    MODIFIED:Added function for validating last name

--- src/main/java/UserValidator.java
+++ src/main/java/UserValidator.java
@@ -3,11 +3,20 @@ import java.util.regex.Pattern;
 
 public class UserValidator
 {
+    // Method for validating first name using Regex
     public boolean validateFirstName(String firstName)
     {
         String patternForFirstName="^[A-Z]{1}[a-z]{2,}$";
         Pattern p = Pattern.compile(patternForFirstName);
         Matcher matcher = p.matcher(firstName);
+        return matcher.matches();
+    }
+    // Method for validating first name using Regex
+    public boolean validateLastName(String lastName)
+    {
+        String patternForLastName="^[A-Z]{1}[a-z]{2,}$";
+        Pattern p = Pattern.compile(patternForLastName);
+        Matcher matcher = p.matcher(lastName);
         return matcher.matches();
     }
 }
