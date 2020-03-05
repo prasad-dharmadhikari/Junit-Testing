2610951e1131d092f7ea56459ac74d4f5acba80e
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-05 13:08:39 +0530

    ADDED:Welcome message

---
+++ src/main/java/UserValidator.java
@@ -0,0 +1,10 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserValidator
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Junit testing");
+    }
+}
