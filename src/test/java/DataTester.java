fb37f408d243888d435652831c90b86f042eba79
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-05 14:51:49 +0530

    ADDED:Test case for validating last name

--- src/test/java/DataTester.java
+++ src/test/java/DataTester.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 public class DataTester
 {
     UserValidator validator = new UserValidator();
+    // Test case for validating first name
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue()
     {
         boolean result = validator.validateFirstName("Prasad");
+        Assert.assertEquals(true,result);
+    }
+    // Test case for validating first name
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue()
+    {
+        boolean result = validator.validateLastName("Dharmadhikari");
         Assert.assertEquals(true,result);
     }
 }
