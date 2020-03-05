8049d74b605aa2041b24e86863c67fea41620a36
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-05 15:42:01 +0530

    MODIFIED:Added function to validate email id

--- src/main/java/UserValidator.java
+++ src/main/java/UserValidator.java
@@ -11,12 +11,20 @@ public class UserValidator
         Matcher matcher = p.matcher(firstName);
         return matcher.matches();
     }
-    // Method for validating first name using Regex
+    // Method for validating last name using Regex
     public boolean validateLastName(String lastName)
     {
         String patternForLastName="^[A-Z]{1}[a-z]{2,}$";
         Pattern p = Pattern.compile(patternForLastName);
         Matcher matcher = p.matcher(lastName);
+        return matcher.matches();
+    }
+    // Method for validating email ID using Regex
+    public boolean validateEmailId(String emailId)
+    {
+        String patternForEmailId="^[a-z]{1,}([.]?[-]?[+]?[a-z0-9]{1,})?[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,4}([.]?[a-z]{2,4})?$";
+        Pattern p = Pattern.compile(patternForEmailId);
+        Matcher matcher = p.matcher(emailId);
         return matcher.matches();
     }
 }
