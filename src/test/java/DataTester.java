1c247fae1bc3fbca421e9c210255742e369bb9e3
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-05 15:43:20 +0530

    ADDED:Test case for validating email id

--- src/test/java/DataTester.java
+++ src/test/java/DataTester.java
@@ -5,18 +5,26 @@ import org.junit.Test;
 public class DataTester
 {
     UserValidator validator = new UserValidator();
+    boolean result=true;
     // Test case for validating first name
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue()
     {
-        boolean result = validator.validateFirstName("Prasad");
+        result = validator.validateFirstName("Prasad");
         Assert.assertEquals(true,result);
     }
-    // Test case for validating first name
+    // Test case for validating last name
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue()
     {
-        boolean result = validator.validateLastName("Dharmadhikari");
+        result = validator.validateLastName("Dharmadhikari");
+        Assert.assertEquals(true,result);
+    }
+    // Test case for validating email ID
+    @Test
+    public void givenEmailId_WhenProper_ShouldReturnTrue()
+    {
+        result = validator.validateEmailId("abc@yahoo.com");
         Assert.assertEquals(true,result);
     }
 }
